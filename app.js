a141323df40c964425d4fa7d8350ed4e4fb5071e
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-16 00:46:05 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+let obj = {};
+let fruit = prompt("กรุณาใส่ชื่อผลไม้ หรือพิมพ์ stop");
+
+while(fruit.toLowerCase() !== 'stop'){
+let num = +prompt(`กรุณาใส่ value ของ ${fruit}`);
+if( num > 1){
+    fruit = fruit + 's'
+  }
+obj[fruit] = num;
+fruit = prompt("กรุณาใส่ชื่อผลไม้ หรือพิมพ์ stop");
+}
+
+console.log('Object:',obj);
+
+for (let fruit in obj) {
+  alert(`ผลไม้: ${fruit}, จำนวน: ${obj[fruit]} หน่วย`);
+}
